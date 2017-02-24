243d317371e23ca05081419bc33bbb22feddcd68
EdgarAllenPoe <tomstoutster@gmail.com>
2017-02-24 20:28:33 -0300

    image updates

--- gulpfile.js
+++ gulpfile.js
@@ -8,23 +8,35 @@ gulp.task('default', function () {
     .pipe(foreach(function(stream, file){
       return stream
            .pipe(stachio({
+             id: "home",
              homeSlider: ([
                 {
                   slideState: "active",
-                  slideHeaderOne: "XWelcome to",
+                  slideHeaderOne: "Bienvenido a",
                   slideHeaderTwo: "SIM Parguay",
-                  slideTagline: "Bootstrap - Responsive Design - Retina Ready - Parallax",
-                  slideImage: "1.jpg"
+                  slideTagline: "Sociedad Internacional Misionera en el Paraguay",
+                  slideImage: "1.jpg",
+                  buttonText: "Comenzar ahora"
                 },
                 {
                   slideState: "",
-                  slideHeaderOne: "YWelcome to",
+                  slideHeaderOne: "Con Una Mision",
                   slideHeaderTwo: "SIM Parguay",
-                  slideTagline: "Bootstrap - Responsive Design - Retina Ready - Parallax",
-                  slideImage: "2.jpg"
-                }
+                  slideTagline: "... para que reciban, por la fe en Mí, el perdón de pecados ...",
+                  slideImage: "2.jpg",
+                  buttonText: "Comenzar ahora"
+                },
+                {
+                  slideState: "",
+                  slideHeaderOne: "Juntos Somos",
+                  slideHeaderTwo: "SIM Parguay",
+                  slideTagline: "... por preservar la unidad del Espíritu en el vínculo de la paz.",
+                  slideImage: "3.jpg",
+                  buttonText: "Comenzar ahora"
+                },
               ]),
              services: ({
+               id: "services",
                header: "XOur Services",
                tagline: "XLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam",
                serviceList: ([
@@ -61,6 +73,7 @@ gulp.task('default', function () {
                 ])
              }),
              aboutUs: ({
+               id:"SIMPY",
                header: "XAbout us",
                tagline: "<p>XLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.Ullamco laboris nisi ut aliquip ex ea commodo consequat. </p><p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
                skillList: ([
@@ -83,8 +96,100 @@ gulp.task('default', function () {
                 ])
              }),
              portfolio: ({
-               captain: "Tom",
-               member1: "Kelly"
+               id: "portfolio",
+               name: "xPortfolio",
+               tagline: "XLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam",
+               project: ([
+                 {
+                   imageOne: "1.jpg",
+                   imageTwo: "1.jpg",
+                   title: "Time Hours",
+                   category: "Design, Photography",
+                   header: "Texts about Project",
+                   desc: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>",
+                   detailOne: "WrapBootrap",
+                   detailTwo: "15 Oct 2014",
+                   detailThree: "Graphic, Design, Creative"
+                 },
+                 {
+                   imageOne: "2.jpg",
+                   imageTwo: "2.jpg",
+                   title: "Time Hours",
+                   category: "Design, Photography",
+                   header: "Texts about Project",
+                   desc: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>",
+                   detailOne: "WrapBootrap",
+                   detailTwo: "15 Oct 2014",
+                   detailThree: "Graphic, Design, Creative"
+                 },
+                 {
+                   imageOne: "3.jpg",
+                   imageTwo: "3.jpg",
+                   title: "Time Hours",
+                   category: "Design, Photography",
+                   header: "Texts about Project",
+                   desc: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>",
+                   detailOne: "WrapBootrap",
+                   detailTwo: "15 Oct 2014",
+                   detailThree: "Graphic, Design, Creative"
+                 },
+                 {
+                   imageOne: "4.jpg",
+                   imageTwo: "4.jpg",
+                   title: "Time Hours",
+                   category: "Design, Photography",
+                   header: "Texts about Project",
+                   desc: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>",
+                   detailOne: "WrapBootrap",
+                   detailTwo: "15 Oct 2014",
+                   detailThree: "Graphic, Design, Creative"
+                 },
+                 {
+                   imageOne: "5.jpg",
+                   imageTwo: "5.jpg",
+                   title: "Time Hours",
+                   category: "Design, Photography",
+                   header: "Texts about Project",
+                   desc: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>",
+                   detailOne: "WrapBootrap",
+                   detailTwo: "15 Oct 2014",
+                   detailThree: "Graphic, Design, Creative"
+                 },
+                 {
+                   imageOne: "6.jpg",
+                   imageTwo: "6.jpg",
+                   title: "Time Hours",
+                   category: "Design, Photography",
+                   header: "Texts about Project",
+                   desc: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>",
+                   detailOne: "WrapBootrap",
+                   detailTwo: "15 Oct 2014",
+                   detailThree: "Graphic, Design, Creative"
+                 },
+                 {
+                   imageOne: "7.jpg",
+                   imageTwo: "7.jpg",
+                   title: "Time Hours",
+                   category: "Design, Photography",
+                   header: "Texts about Project",
+                   desc: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>",
+                   detailOne: "WrapBootrap",
+                   detailTwo: "15 Oct 2014",
+                   detailThree: "Graphic, Design, Creative"
+                 },
+                 {
+                   imageOne: "8.jpg",
+                   imageTwo: "8.jpg",
+                   title: "Time Hours",
+                   category: "Design, Photography",
+                   header: "Texts about Project",
+                   desc: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>",
+                   detailOne: "WrapBootrap",
+                   detailTwo: "15 Oct 2014",
+                   detailThree: "Graphic, Design, Creative"
+                 }
+               ]),
+
              }),
              team: ({
                captain: "Tom",
